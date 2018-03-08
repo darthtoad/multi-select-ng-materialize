0a2ea9abc3002a0add204108331950fd44d4ea89
Guest User <Guest@Epicodus-3C.local>
2018-03-07 16:32:25 -0800

    get multiselect to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MaterializeDirective} from "angular2-materialize";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  selectedOptions = "";
+
+  selectOptions = [];
+
+  public ngOnInit() {
+      window.setTimeout(()=>{
+        this.selectOptions = [
+          {value:1,name:"Option 1"},
+          {value:2,name:"Option 2"},
+          {value:3,name:"Option 3"}
+        ]
+      },100);
+
+      // window.setInterval(()=>{
+      //   console.log(this.radioButtonValue);
+      // },500);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterializeModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
